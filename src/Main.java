cf0bae1455428312b19560ec9faeff0db4b1e71a
Denis Koryakin <singler1991@gmail.com>
2022-12-28 12:50:24 +0500

    correction functional interfaces

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
+        int c = calc.divide.apply(a, b);
         calc.println.accept(c);
     }
 }
